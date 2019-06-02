04d0df636e427d23c9904bf2aae0c3c146c163c9
jahknem <jan.kuehnemund@outlook.com>
2019-06-02 18:21:23 +0200

    Enter

---
+++ RDM53/src/protocol.cpp
@@ -0,0 +1,33 @@
+/*
+ *This file contains all the protocol functions for the ESP32
+ *
+ *
+ *Author: Jan Kühnemund
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char* protocolEnter(char* incoming, char calibrationID)
+{
+    if(incoming[2]== 3) //receiveData
+    {
+        if(calibrationID == incoming[4]){
+            char datavalue[4];
+            datavalue[1] = incoming[6];
+            datavalue[2] = incoming[7];
+            datavalue[3] = incoming[8];
+            datavalue[4] = incoming[9];
+            return datavalue;
+        }
+    }
+    return NULL;
+}
+
+char* protocolExit(int dataTypeID, int dataSourceID, double payload) //sendData
+{
+    char *exiting = (char*)malloc(sizeof(char) * 15);
+    int n;
+    n = sprintf(exiting, "01%x%x%x", dataTypeID, dataSourceID, payload);
+    return exiting;
+}
