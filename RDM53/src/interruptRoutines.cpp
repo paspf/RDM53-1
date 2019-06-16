016dec6c913e93ba028a45d31112f82806d691c1
Pascal Pfeiffer <Pascal.pfeiffer@iem.thm.de>
2019-06-16 14:45:27 +0200

    Comments Added

--- RDM53/src/interruptRoutines.cpp
+++ RDM53/src/interruptRoutines.cpp
@@ -9,25 +9,39 @@
 // Critical section management
 portMUX_TYPE mux = portMUX_INITIALIZER_UNLOCKED;
 
+/*
+ * Interrupt Service Routine for Pin 19
+ */
 void IRAM_ATTR handleInterruptP19() {
   portENTER_CRITICAL_ISR(&mux);
   interruptCounterKey1++;
   portEXIT_CRITICAL_ISR(&mux);
 }
 
+/*
+ * Interrupt Service Routine for Pin 18
+ */
 void IRAM_ATTR handleInterruptP18() {
   portENTER_CRITICAL_ISR(&mux);
   interruptCounterKey2++;
   portEXIT_CRITICAL_ISR(&mux);
 }
- 
+
+/*
+ * Initialize Interrupts
+ * Configure External Interrupt Pins
+ */
 void interruptInitialization() {
   pinMode(KEY1, INPUT_PULLUP);
   pinMode(KEY2, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(KEY1), handleInterruptP19, FALLING);
   attachInterrupt(digitalPinToInterrupt(KEY2), handleInterruptP18, FALLING);
 }
- 
+
+/*
+ * Do stuff that should be run if an Interrupt has occured
+ * This function is OUTSIDE of the interrupt itself
+ */
 void interruptWorkers() {
  
   if(interruptCounterKey1 > 0){
