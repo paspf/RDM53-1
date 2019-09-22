7359b0c90c295b029c444541ad4200af89bf5474
Jan <33702431+jahknem@users.noreply.github.com>
2019-09-22 13:28:35 +0200

    Ease of Use
    Added ease of use to choosing document

--- MagneticCalibration/MagCalibration.m
+++ MagneticCalibration/MagCalibration.m
@@ -1,6 +1,25 @@
 close all
 clearvars
-fid = fopen('log.txt', 'r');
+
+% Have user browse for a file, from a specified "starting folder."
+% For convenience in browsing, set a starting folder from which to browse.
+userProfile = getenv('USERPROFILE');
+myDocsFolder = sprintf('%s\\My Documents', userProfile);
+startingFolder = myDocsFolder;
+if ~exist(startingFolder, 'dir')
+  % If that folder doesn't exist, just start in the current folder.
+  startingFolder = pwd;
+end
+% Get the name of the file that the user wants to use.
+defaultFileName = fullfile(startingFolder, '*.txt*');
+[baseFileName, folder] = uigetfile(defaultFileName, 'Select a file');
+if baseFileName == 0
+  % User clicked the Cancel button.
+  return;
+end
+fullFileName = fullfile(folder, baseFileName);
+
+fid = fopen(fullFileName, 'r');
 formatSpec = '%f';
 sizeA = [3 Inf];
 A = fscanf(fid,formatSpec,sizeA);
